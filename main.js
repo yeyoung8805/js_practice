64094e097632ff6bd47625e50efa2e96ebb58ea7
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:42:20 +0900

    arrow  function

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-// 함수 복습
+// 화살표 함수
+// () => {}  vs  function () {}
 
-function sum(x, y) {
-  return x + y;
+//일반 익명함수
+const double = function (x) {
+  return x * 2;
 }
+console.log('double: ', double(7));
 
-console.log(sum(7, 3)); //매개변수 x,y를 사용해서 명시해주는게 권장하는 방법이긴 하다.
+//화살표 익명함수
+//기본적인 로직을 축약형으로 줄일수 있다는 차이점이 있다!
+const doubleArrow = (x) => {
+  return x * 2;
+}
+console.log('doubleArrow', doubleArrow(7));
