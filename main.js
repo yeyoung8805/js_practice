dd54a0651a706de82ff27feeb88ce6714f6e18c6
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:10:33 +0900

    falsy undefined

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 // Truthy (참 같은 값)
 // true, {}, [], 1, 2, 'false', -12, '3.14' ...
 
-// Falsy (거짓 같은 값)
+// Falsy (거짓 같은 값) : 굉장히 많은 truthy 보다 개수가 훨씬 적다. 제한적이다.
 // false, '', null, undefined, 0, -0, NaN (Not a Number)
 
-if('false') { //truthy
+if(undefined) { //Falsy
   console.log(123);
 }
