026a174974ef60375dbb55174653c07c18989ab9
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:18:42 +0900

    not efficient way

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ function sum(x, y) { //x,y 라는 매개변수
 const a = sum(1, 3); // 1,3 이라는 인수
 const b = sum(4, 12);
 
-console.log(a);
-console.log(b);
-console.log(a + b);
+console.log(sum(1, 3));
+console.log(sum(4, 12));
+console.log(sum(1, 3) + sum(4, 12));
