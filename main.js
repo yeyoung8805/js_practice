7a8139714c7c31a90299053b1d37d7fc099bc58f
yeyoung8805 <yyk8805@naver.com>
2022-08-04 18:12:02 +0900

    add comment to IIFE

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // 즉시실행함수
 // IIFE, Immediately-Invoked Function Expression
 
+
+//기존 방식
 const a = 7;
 function double() {
   console.log(a * 2);
@@ -8,12 +10,13 @@ function double() {
 double();
 
 
-//즉시실행함수 의 형식이다!
+//1. 즉시실행함수 의 형식이다!
 (function () {
   console.log(a * 2);
 }) ();
 
-//즉시실행함수 의 다른 형태이다! (위와 똑같은 기능을 한다.)
+//2. 즉시실행함수 의 다른 형태이다! (위와 똑같은 기능을 한다.)
+//1.보다 2.방법이 더 권장된다.
 (function () {
   console.log(a * 2);
 }());
