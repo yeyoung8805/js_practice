f3d31308a81245618d633b98ce0e6be9a2516977
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:29:19 +0900

    return; keyword

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 // 함수 복습
 
-//익명함수
-const sum = function (x, y) { //x,y 라는 매개변수
+function sum(x, y) {
+  if(x < 2) {
+    return; // x값이 2보다 작으면 여기서 종료된다. 밑의 내용들 실행하지 않는다. 함수가 여기서 종료된다.
+  }
   return x + y;
-  console.log(x); //return 이후이므로 함수기능이 종료됐기 때문에, 개발자도구 콘솔에 log 가 찍히지 않는다.
 }
 
-const a = sum(1, 3); // 1,3 이라는 인수
-const b = sum(4, 12);
-
-console.log(a);
-console.log(b);
-console.log(a + b); //함수 계속 호출하는 비효율적인 구조
+console.log(sum(1, 3)); //result : undefined
