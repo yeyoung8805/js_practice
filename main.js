f3d89551cd7070bc0b25afb87fd2e4fafba8eec2
yeyoung8805 <yyk8805@naver.com>
2022-08-04 14:33:00 +0900

    operator &&

--- main.js
+++ main.js
@@ -7,3 +7,6 @@ const c = true;
 console.log(a); //result true
 console.log(b); //result true
 console.log(c); //result true
+
+console.log(a && b && c); //&&는 And 연산자로서, 하나라도 false이면 result false가 됨. 모두 true가 되어야 함
+console.log('&&: ', a && b && c); //문장이 길어질 경우 구분자를 앞에 '' 넣음
