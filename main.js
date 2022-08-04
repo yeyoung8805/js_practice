335c179f6dafb87869fe009ada42d620396ebc7f
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:22:38 +0900

    anonymous function

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // 함수 복습
 
-function sum(x, y) { //x,y 라는 매개변수
+//익명함수
+const sum = function (x, y) { //x,y 라는 매개변수
   return x + y;
 }
 
 const a = sum(1, 3); // 1,3 이라는 인수
 const b = sum(4, 12);
 
-console.log(sum(1, 3));
-console.log(sum(4, 12));
-console.log(sum(1, 3) + sum(4, 12)); //함수 계속 호출하는 비효율적인 구조
+console.log(a);
+console.log(b);
+console.log(a + b); //함수 계속 호출하는 비효율적인 구조
