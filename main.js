6a1775bbb350c21f8c1a33eda1b46e8b20c72143
yeyoung8805 <yyk8805@naver.com>
2022-08-04 11:25:54 +0900

    function isEqual()

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const a = 1;
 const b = 3;
 
 console.log(a === b); //result : false  // 일치연산자 ===
+
+function isEqual(x,y) {
+  return x === y;
+}
+
+console.log(isEqual(1,1)); //true
+console.log(isEqual(2,'2')); //false number, string is different
