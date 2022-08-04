b13dfeacc922f3b6b97014c6cdf3ab84342dd153
yeyoung8805 <yyk8805@naver.com>
2022-08-04 14:34:36 +0900

    operator ||

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const a = 1 === 1;
 const b = 'AB' === 'AB';
-const c = true;
+const c = false;
 
 console.log(a); //result true
 console.log(b); //result true
@@ -10,3 +10,5 @@ console.log(c); //result true
 
 console.log(a && b && c); //&&는 And 연산자로서, 하나라도 false이면 result false가 됨. 모두 true가 되어야 함
 console.log('&&: ', a && b && c); //문장이 길어질 경우 구분자를 앞에 '' 넣음
+
+console.log('||: ', a || c); //||는 Or 연산자. 하나라도 true 이면 true
