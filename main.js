782ecad99a207ff9315a9af58371ea5523e0f0d7
yeyoung8805 <yyk8805@naver.com>
2022-08-04 14:58:59 +0900

    Switch statement

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 import randomFunc from './getRandom';
 
-//조건문 (If statement)
+//조건문 (Switch statement)
 
 const a = randomFunc();
 
-if(a === 0) {
-  console.log('a is 0');
-} else if(a === 2) {
-  console.log('a is 2');
-} else if(a === 4) {
-  console.log('a is 4');
-} else {
-  console.log('rest...');
+switch(a) {
+  case 0 :
+    console.log('a is 0');
+    break;
+  case 2 :
+    console.log('a is 2');
+    break;
+  case 4 :
+    console.log('a is 4');
+    break;
+  default :
+    console.log('rest...');
+    break;
 }
