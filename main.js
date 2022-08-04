b75dbf3015a7044bba5e41f2b98dbf65ce35d29e
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:09:38 +0900

    truthy String

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //형 변환(Type conversion)
 
-// Truthy (참같은 값)
+// Truthy (참 같은 값)
 // true, {}, [], 1, 2, 'false', -12, '3.14' ...
 
-// Falsy (거짓같은 값)
-// false, '', null, undefined, 0, -0, NaN
+// Falsy (거짓 같은 값)
+// false, '', null, undefined, 0, -0, NaN (Not a Number)
 
-if(true) {
+if('false') { //truthy
   console.log(123);
 }
