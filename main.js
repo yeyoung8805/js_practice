3932824bbacce47a467d1560d245ca8f07a709cc
yeyoung8805 <yyk8805@naver.com>
2022-08-04 15:10:56 +0900

    appendChild to ul

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 //for ( 시작조건; 종료조건; 변화조건) {}
 
 const ulEl = document.querySelector('ul');
-console.log(ulEl);
 
 for(let i = 0; i < 3; i += 1) {
-  console.log(i);
+  const li = document.createElement('li'); //메모리 상에 li 를 만든 상태(눈에 보이진 않지만)
+  li.textContent = `list-${i + 1}`; //li의 내용물로 list-1, list-2, list-3 나오게 함
+  ulEl.appendChild(li); //ul 안에 child로 li 들 추가함
 }
