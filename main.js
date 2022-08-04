77421ce4a77bbdf7ad5845de6c0d4d59ff18f808
yeyoung8805 <yyk8805@naver.com>
2022-08-04 14:40:17 +0900

    ternary operator

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-// 논리 연산자(logical operator)
+//삼항 연산자(ternary operator)
 
-const a = 1 === 123;
-const b = 'AB' === 'AB';
-const c = false;
+const a = 1 < 2; //true
 
-console.log(a); //result true
-console.log(b); //result true
-console.log(c); //result true
-
-console.log(a && b && c); //&&는 And 연산자로서, 하나라도 false이면 result false가 됨. 모두 true가 되어야 함
-console.log('&&: ', a && b && c); //문장이 길어질 경우 구분자를 앞에 '' 넣음
-
-console.log('||: ', a || c); //||는 Or 연산자. 하나라도 true 이면 true
-
-console.log('!: ', !a); //!는 Not 부정 연산자. 기존값의 반대가 된다.
+if(a) {
+  console.log('참');
+}else {
+  console.log('거짓');
+}
