a8eec7b50cee3ca2ff53de9e10d19d0f98711c24
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:32:58 +0900

    console.log(arguments); without parameters

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 // 함수 복습
 
-function sum(x, y) {
-  if(x < 2) {
-    return; // x값이 2보다 작으면 여기서 종료된다. 밑의 내용들 실행하지 않는다. 함수가 여기서 종료된다.
-  }
-  return x + y;
+function sum() {
+  console.log(arguments);
+  return;
 }
 
-console.log(sum(1, 3)); //result : undefined
+console.log(sum(7, 3)); //Arguments 배열에 7, 3이 각각 0번째, 1번째로 들어간다.
