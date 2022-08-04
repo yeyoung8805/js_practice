462c198092a5bd264d12464d98ffbbe8c64bd50a
yeyoung8805 <yyk8805@naver.com>
2022-08-04 18:00:04 +0900

    IIFE, Immediately-Invoked Function Expression

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-// 화살표 함수
-// () => {}  vs  function () {}
+// 즉시실행함수
+// IIFE, Immediately-Invoked Function Expression
 
-//일반 익명함수
-const double = function (x) {
-  return x * 2;
+const a = 7;
+function double() {
+  console.log(a * 2);
 }
-console.log('double: ', double(7));
-
-//화살표 익명함수
-//기본적인 로직을 축약형으로 줄일수 있다는 차이점이 있다!
-const doubleArrow = x => ({ //객체데이터 {}를 ()로 감싸줘야 함수가 아닌 객체로 인식한다.
-  name: 'YeYoung'
-}); // result : object
-console.log('doubleArrow', doubleArrow(7));
+double();
