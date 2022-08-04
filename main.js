cb6fc2981742f7907e7a10178f87846d30c6a700
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:07:41 +0900

    Truthy

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //형 변환(Type conversion)
 
-const a = 1;
-const b = '1';
+// Truthy (참같은 값)
+// true, {}, [], 1, 2, 'false', -12, '3.14' ...
 
-// Javascript에서는 동등연산자보단 일치연산자를 권장한다.
-console.log(a === b); // 일치 연산자 ===
-console.log(a == b); // 동등 연산자 ==
+// Falsy (거짓같은 값)
+// false, '', null, undefined, 0, -0, NaN
+
+if(true) {
+  console.log(123);
+}
