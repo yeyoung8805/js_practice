b29274757b38ad7502bd76d52b967b40fac4d27b
yeyoung8805 <yyk8805@naver.com>
2022-08-04 18:10:13 +0900

    IIFE, applied version

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ function double() {
   console.log(a * 2);
 }
 double();
+
+
+//즉시실행함수 의 형식이다!
+(function () {
+  console.log(a * 2);
+}) ();
+
+//즉시실행함수 의 다른 형태이다! (위와 똑같은 기능을 한다.)
+(function () {
+  console.log(a * 2);
+}());
