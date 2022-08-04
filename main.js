0be515a48a2011d8b546a57e50c8b4a119f57876
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:16:07 +0900

    applying sum function

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // 함수 복습
 
 function sum(x, y) { //x,y 라는 매개변수
-  console.log(x + y);
+  return x + y;
 }
 
-sum(1, 3); // 1,3 이라는 인수
-sum(4, 12);
+const a = sum(1, 3); // 1,3 이라는 인수
+const b = sum(4, 12);
+
+console.log(a);
+console.log(b);
