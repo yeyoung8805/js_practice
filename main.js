fcc8efe93011f577f2704da4d9199a6d1c1d56ef
yeyoung8805 <yyk8805@naver.com>
2022-08-04 14:38:04 +0900

    operator ! Not

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // 논리 연산자(logical operator)
 
-const a = 1 === 1;
+const a = 1 === 123;
 const b = 'AB' === 'AB';
 const c = false;
 
@@ -12,3 +12,5 @@ console.log(a && b && c); //&&는 And 연산자로서, 하나라도 false이면 
 console.log('&&: ', a && b && c); //문장이 길어질 경우 구분자를 앞에 '' 넣음
 
 console.log('||: ', a || c); //||는 Or 연산자. 하나라도 true 이면 true
+
+console.log('!: ', !a); //!는 Not 부정 연산자. 기존값의 반대가 된다.
