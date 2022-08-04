d8610403cb90a9045e2648791a1d7558f6e921bc
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:43:53 +0900

    abbreviated arrow function

--- main.js
+++ main.js
@@ -9,7 +9,5 @@ console.log('double: ', double(7));
 
 //화살표 익명함수
 //기본적인 로직을 축약형으로 줄일수 있다는 차이점이 있다!
-const doubleArrow = (x) => {
-  return x * 2;
-}
+const doubleArrow = (x) => x * 2;
 console.log('doubleArrow', doubleArrow(7));
