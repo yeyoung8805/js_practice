165cf5a95a3a3eeb8c1168efe528726a79c09f6b
yeyoung8805 <yyk8805@naver.com>
2022-08-04 11:23:19 +0900

    change the value of const b

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // 비교 연산자(comparison operator)
 
 const a = 1;
-const b = 1;
+const b = 3;
 
-console.log(a === b); //result : true  // 일치연산자 ===
+console.log(a === b); //result : false  // 일치연산자 ===
