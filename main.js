89ef665c72c429edcfca1de0de0910d49960b90f
yeyoung8805 <yyk8805@naver.com>
2022-08-04 17:45:53 +0900

    abbreviate () parenthesis

--- main.js
+++ main.js
@@ -9,5 +9,5 @@ console.log('double: ', double(7));
 
 //화살표 익명함수
 //기본적인 로직을 축약형으로 줄일수 있다는 차이점이 있다!
-const doubleArrow = (x) => x * 2;
+const doubleArrow = x => x * 2; //매개변수가 하나일 경우 소괄호도 생략 가능
 console.log('doubleArrow', doubleArrow(7));
