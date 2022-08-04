7785eaefeec3c400e9d2c599779879ac7ba1324e
yeyoung8805 <yyk8805@naver.com>
2022-08-04 11:28:54 +0900

    change const b to 1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // 비교 연산자(comparison operator)
 
 const a = 1;
-const b = 3;
+const b = 1;
 
-console.log(a !== b); //result true //서로가 다른 것인지를 확인해주는 연산자 !==
+console.log(a !== b); //result false //서로가 다른 것인지를 확인해주는 연산자 !==
