bab2cd520c2826fec5b761e7c808c5ce40e02dd5
yeyoung8805 <yyk8805@naver.com>
2022-08-04 11:15:11 +0900

    plus 1 to let a

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // 할당 연산자(assignment operator)
 
-const a = 2; // = 가 할당연산자
-let b = 2; //const는 재할당 불가, let은 재할당 가능 
-console.log(b);
+let a = 2; //const는 재할당 불가, let은 재할당 가능
+a = a + 1;
+console.log(a);
