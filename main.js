e712693be2554b5774b883a386c21e83bc375a11
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 10:06:18 +0900

    clearInterval function

--- main.js
+++ main.js
@@ -4,11 +4,11 @@
 // clearTimeout() : 설정된 Timeout 함수를 종료
 // clearInterval() : 설정된 Interval 함수를 종료
 
-const timer = setTimeout(() => { //화살표 함수 
+const timer = setInterval(() => { //화살표 함수 
   console.log('YeYoung');
 }, 3000);
 
 const h1El = document.querySelector('h1');
 h1El.addEventListener('click', () => {
-  clearTimeout(timer); //브라우저 새로고침(F5)하고 3초가 되기전 h1 태그내용을 클릭하면, console에 찍히는 값이 없게 된다.
+  clearInterval(timer); //3초마다 콘솔에 찍히는데, 그 중간에 h1의 내용을 클릭하게 되면 더이상 콘솔찍히지 않게 된다.
 });
