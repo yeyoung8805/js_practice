51d60ce9362b5c345e455f0f681361e4854cdba0
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 16:11:36 +0900

    this keyword undefined in normal function

--- main.js
+++ main.js
@@ -2,19 +2,13 @@
 //일반(Normal) 함수는 '호출 위치에서' 따라 this 정의!
 //화살표(Arrow) 함수는 '자신이 선언된 함수 범위에서'(블록 내에서) this 정의!
 
-function User(name) {
-  this.name = name;
-}
-
-User.prototype.normal = function () {
-  console.log(this.name);
-}
-
-User.prototype.arrow = () => {
-  console.log(this.name);
-}
-
-const heropy = new User('Heropy');
-
-heropy.normal(); //result : Heropy
-heropy.arrow(); //result : undefined
+const timer = {
+  name: 'Heropy!!',
+  timeout: function () {
+    //setTimeout(함수,시간)
+    setTimeout(function() { //일반함수 내의 this는 setTimeout()내의 범위에서만 찾으므로 name에 도달할 수 없다!!
+      console.log(this.name);
+    }, 2000);
+  }
+};
+timer.timeout();
