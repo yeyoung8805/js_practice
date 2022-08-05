a234fd7f5c6a0b117136711bffae1f93e4433916
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 14:52:14 +0900

    name it as Pascal case

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-function user(first, last) {
+function User(first, last) { //생성자 함수는 파스칼 케이스 사용하여 대문자로 표시
   this.firstName = first;
   this.lastName = last;
 };
-user.prototype.getFullName = function () { //prototype 개념이 도입된다.
+User.prototype.getFullName = function () { //prototype 개념이 도입된다.
   return `${this.firstName} ${this.lastName}`;
 }
 
-const yeyoung = new user('Yeyoung', 'Kim'); // const yeyoung 은 인스턴스(instance)이다!
-const amy = new user('Amy', 'Clarke');
-const neo = new user('Neo', 'Smith');
+const yeyoung = new User('Yeyoung', 'Kim'); // const yeyoung 은 인스턴스(instance)이다!
+const amy = new User('Amy', 'Clarke');
+const neo = new User('Neo', 'Smith');
 
 console.log(yeyoung.getFullName());
 console.log(amy.getFullName());
