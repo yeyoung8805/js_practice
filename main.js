e20486139ec21a2ca5c9411c8b35e68c56bc9aa4
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 16:41:57 +0900

    example of using extends keyword

--- main.js
+++ main.js
@@ -4,16 +4,28 @@ class Vehicle {
     this.wheel = wheel;
   }
 }
+const myVehicle = new Vehicle('운송수단', 2);
+console.log(myVehicle);
+
 
 class Bicycle extends Vehicle {
   constructor(name, wheel) {
-    super(name, wheel);
+    super(name, wheel); //Vehicle 클래스를 의미
   }
 }
+const myBicycle = new Bicycle('삼천리', 2);
+const daughtesrBicycle = new Bicycle('세발', 3);
+console.log(myBicycle);
+console.log(daughtesrBicycle);
+
 
 class Car extends Vehicle {
   constructor(name, wheel, license) {
-    super(name, wheel);
-    this.license = license;
+    super(name, wheel); //Vehicle 클래스를 의미
+    this.license = license; //추가적인 내용 확장(상속)
   }
 }
+const myCar = new Car('현대', 4, true);
+const daughtersCar = new Car('기아', 4, false);
+console.log(myCar);
+console.log(daughtersCar);
