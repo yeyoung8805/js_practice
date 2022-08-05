10ee18b7f929be2dd609d0125dd3ad916e87d66f
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 14:50:38 +0900

    prototype usage

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ function user(first, last) {
   this.firstName = first;
   this.lastName = last;
 };
+user.prototype.getFullName = function () { //prototype 개념이 도입된다.
+  return `${this.firstName} ${this.lastName}`;
+}
 
 const yeyoung = new user('Yeyoung', 'Kim'); // const yeyoung 은 인스턴스(instance)이다!
 const amy = new user('Amy', 'Clarke');
 const neo = new user('Neo', 'Smith');
 
-console.log(yeyoung);
-console.log(amy);
-console.log(neo);
+console.log(yeyoung.getFullName());
+console.log(amy.getFullName());
+console.log(neo.getFullName());
