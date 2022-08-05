0489f2a4359e53efdc51bc41910824f814f8071d
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 17:50:22 +0900

    using regExp in match()

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-const str = 'Hello world!';
+const str = 'world@gmail.com';
 
-console.log(str.replace(' world!', '')); 
+console.log(str.match(/.+(?=@)/));  //정규표현식(RegExp) 사용
+console.log(str.match(/.+(?=@)/)[0]); //배열의 0 키에 들어간 value "world"만 추출
