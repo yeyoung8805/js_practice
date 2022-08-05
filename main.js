240c71ffb29402bcb15ab3ee8f6898770604c348
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 17:53:44 +0900

    String trim()

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const str = 'world@gmail.com';
+const str = '     Hello world   ';
 
-console.log(str.match(/.+(?=@)/));  //정규표현식(RegExp) 사용
-console.log(str.match(/.+(?=@)/)[0]); //배열의 0 키에 들어간 value "world"만 추출
+console.log(str.trim()); //trim()을 통해 모든 공백문자 제거
