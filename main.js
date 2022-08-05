db96c5256a61234a6e8f1757bf7c1454439e17fb
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 18:02:26 +0900

    toFixed(), parseInt(), parseFloat()

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-const str = '     Hello world   ';
+const pi = 3.14159265358979;
+console.log(pi); //result : 3.14159265358979
 
-console.log(str.trim()); //trim()을 통해 모든 공백문자 제거
+const str = pi.toFixed(2);
+console.log(str); //result : 3.14  //소수점 2자리까지만 남기고 다 버림
+console.log(typeof str); //result : String
+
+const integer = parseInt(str);
+const float = parseFloat(str); 
+console.log(integer); //result : 3  //정수로 반환 
+console.log(float); //result : 3.14  //소수점숫자도 유지하면서 문자를 숫자로 변환
+console.log(typeof integer, typeof float); //result : number, number
