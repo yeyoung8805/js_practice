07a44060c1b3910e7650aed2d166c94f918dc4f7
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 16:28:42 +0900

    example usage of ES6 Class

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 //ES6 Classes
 
-function User(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-}
+// function User(first, last) {
+//   this.firstName = first;
+//   this.lastName = last;
+// }
 
-User.prototype.getFullName = function () {
-  return `${this.firstName} ${this.lastName}`;
+// User.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// }
+
+//class 사용한 새로운 문법으로 구현한다!!(React 등도 비슷함)
+class User {
+  constructor(first, last) {
+    this.firstName = first;
+    this.lastName = last; 
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 const heropy = new User('Heropy', 'Kim');
