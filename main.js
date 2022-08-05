1cda4b10e6969cf01a644ab29fb80a3203789d1d
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 13:56:22 +0900

    add method inside

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function user(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.getFullName = function () {
+    return `${this.firstName} ${this.lastName}`;
+  }
 };
 
 const yeyoung = new user('Yeyoung', 'Kim'); //user()는 생성자 함수
 
 console.log(yeyoung);
-
-// const yeyoung1 = {}; // {}로 나타내는 방식이 리터럴 방식
