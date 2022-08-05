6a69cbcad34b74e033e46b008b164b5b1e83105f
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 13:59:13 +0900

    concept of instance

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function user(first, last) {
   this.firstName = first;
   this.lastName = last;
-  this.getFullName = function () {
-    return `${this.firstName} ${this.lastName}`;
-  }
 };
 
-const yeyoung = new user('Yeyoung', 'Kim'); //user()는 생성자 함수
+const yeyoung = new user('Yeyoung', 'Kim'); // const yeyoung 은 인스턴스(instance)이다!
+const amy = new user('Amy', 'Clarke');
+const neo = new user('Neo', 'Smith');
 
 console.log(yeyoung);
+console.log(amy);
+console.log(neo);
