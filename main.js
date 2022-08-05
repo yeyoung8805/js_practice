4309e43f0b29ddefe1be4750a7235a3b6e9a9dd8
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 09:55:50 +0900

    setTimeout function

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-//호이스팅 (Hoisting)
-//함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
+// 타이머 함수
+// setTimeout(함수, 시간) : 일정 시간 후 함수 실행
+// setInterval(함수, 시간) : 시간 간격마다 함수 실행
+// clearTimeout() : 설정된 Timeout 함수를 종료
+// clearInterval() : 설정된 Interval 함수를 종료
 
-const a = 7;
-
-double();
-
-//함수 선언부가 double();호출부보다 아래있어도 유효범위 최상단으로 끌어올려진다.
-function double() {
-  console.log(a * 2);
-}
+setTimeout(function() {
+  console.log('YeYoung');
+}, 3000);
