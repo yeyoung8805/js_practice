4a9e49c123f898a8e120d9af620c0c1d336008a2
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 17:43:13 +0900

    extracting 'world' out of slice()

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const str = 'Hello world!';
 
 //slice(beginIndex, endIndex);  // endIndex is optional
-console.log(str.slice(0, 3)); //result : Hel  //0번째인덱스부터 3번째 "이전"의 인덱스까지 추출한다.
+console.log(str.slice(6, 11)); //result : world
