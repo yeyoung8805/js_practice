9036064fbf8e594c03c568615e544e752b071265
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 18:10:47 +0900

    random class

--- getRandom.js
+++ getRandom.js
@@ -1,3 +1,3 @@
 export default function random() {
-  return Math.floor(Math.random() * 10); // 정수 1 자리수를 받아와서 -> 내림을 하는것(소수점은 버리는것)
+  return Math.floor(Math.random() * 10); // Math.random() * 10 (정수 1 자리수를 받아와서) -> floor () (내림을 하는것(소수점은 버리는것))
 }
