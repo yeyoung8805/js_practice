ff114b1106e7a32684cc093e9f92689b4bcd72a5
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 13:27:49 +0900

    non-efficient repeatant code

--- main.js
+++ main.js
@@ -8,3 +8,25 @@ const yeyoung = {
 };
 
 console.log(yeyoung.getFullName());
+
+
+const amy = {
+  firstName: 'Amy',
+  lastName: 'Clarke',
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  }
+};
+
+console.log(amy.getFullName());
+
+
+const neo = {
+  firstName: 'Neo',
+  lastName: 'Smith',
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  }
+};
+
+console.log(neo.getFullName());
