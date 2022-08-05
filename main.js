bc84e36fa8207ddeaef92fe162e26874bff0c4cb
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 14:58:19 +0900

    this keyword in normal/arrow function

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-function User(first, last) { //생성자 함수는 파스칼 케이스 사용하여 대문자로 표시
-  this.firstName = first;
-  this.lastName = last;
+//this
+//일반(Normal) 함수는 호출 위치에서 따라 this 정의!
+//화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의!
+
+const heropy = {
+  name: 'Heropy',
+  normal: function() {
+    console.log(this.name);
+  },
+  arrow: () => {
+    console.log(this.name);
+  }
 };
-User.prototype.getFullName = function () { //prototype 개념이 도입된다.
-  return `${this.firstName} ${this.lastName}`;
-}
 
-const yeyoung = new User('Yeyoung', 'Kim'); // const yeyoung 은 인스턴스(instance)이다!
-const amy = new User('Amy', 'Clarke');
-const neo = new User('Neo', 'Smith');
-
-console.log(yeyoung.getFullName());
-console.log(amy.getFullName());
-console.log(neo.getFullName());
+heropy.normal(); //result : heropy
+heropy.arrow(); //result : undefined
