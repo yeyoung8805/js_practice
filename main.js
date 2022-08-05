cd94d14a6b57cba572f735aacee5ef881041797b
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 10:18:52 +0900

    callback inside function timeout()

--- main.js
+++ main.js
@@ -3,11 +3,13 @@
 
 // setTimeout(함수, 시간)
 
-function timeout() {
+function timeout(callback) {
   setTimeout(()=> {
     console.log('YeYoung!');
+    callback();
   }, 3000);
 };
-timeout();
+timeout(() => { //timeout() 함수 내에 화살표 함수라는 콜백을 넣는다.
+  console.log('Done!');
+});
 
-console.log('Done!');
