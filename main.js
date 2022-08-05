1564ba9a70e6864251d92aa94827c6307e6ce0f1
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 15:10:16 +0900

    result of prototype normal,arrow function

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 //this
-//일반(Normal) 함수는 호출 위치에서 따라 this 정의!
-//화살표(Arrow) 함수는 자신이 선언된 함수 범위에서 this 정의!
+//일반(Normal) 함수는 '호출 위치에서' 따라 this 정의!
+//화살표(Arrow) 함수는 '자신이 선언된 함수 범위에서'(블록 내에서) this 정의!
 
-const heropy = {
-  name: 'Heropy',
-  normal: function() {
-    console.log(this.name);
-  },
-  arrow: () => {
-    console.log(this.name);
-  }
-};
+function User(name) {
+  this.name = name;
+}
 
-heropy.normal(); //result : heropy
-heropy.arrow(); //result : undefined //일반함수와 달리 화살표함수는 호출된 위치와는 관련이 없다.
+User.prototype.normal = function () {
+  console.log(this.name);
+}
 
-const amy = {
-  name: 'Amy',
-  normal: heropy.normal,
-  arrow: heropy.arrow
-};
-amy.normal(); //result : Amy  //this is name:'Amy'
-amy.arrow(); //result : undefined
+User.prototype.arrow = () => {
+  console.log(this.name);
+}
+
+const heropy = new User('Heropy');
+
+heropy.normal(); //result : Heropy
+heropy.arrow(); //result : undefined
