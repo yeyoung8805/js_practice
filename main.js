0c4e335bd31ca6cde507b75bec3aeb12d99100e6
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 13:23:43 +0900

    constructor function (prototype)

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-// 콜백(Callback)
-// 함수의 인수로 사용되는 함수
-
-// setTimeout(함수, 시간)
-
-function timeout(callback) {
-  setTimeout(()=> {
-    console.log('YeYoung!');
-    callback();
-  }, 3000);
+//생성자 함수 (prototype)
+const yeyoung = {
+  firstName: 'YeYoung',
+  lastName: 'Kim',
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  }
 };
-timeout(() => { //timeout() 함수 내에 화살표 함수라는 콜백을 넣는다.
-  console.log('Done!');
-});
 
+console.log(yeyoung);
