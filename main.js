f3915b5c6a37dafe52bd48718b37ea0de1f0e78e
“yeyoung8805” <yyk8805@naver.com>
2022-08-05 17:41:55 +0900

    add comment to slice()

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 const str = 'Hello world!';
 
-console.log(str.slice(0, 3)); //result : Hel  //0번째인덱스부터 3개의 문자를 반환
+//slice(beginIndex, endIndex);  // endIndex is optional
+console.log(str.slice(0, 3)); //result : Hel  //0번째인덱스부터 3번째 "이전"의 인덱스까지 추출한다.
