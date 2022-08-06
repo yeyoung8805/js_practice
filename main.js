da367b77a4e029358a25dc5abc1ad52c7cdce5f3
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:24:45 +0900

    array find()

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-//.filter()
+//.find() .findIndex()
 
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-const a = numbers.map(numbers =>number < 3); //return에 하나의 실행문만 반환할경우 줄여서 표현할 수 있다.
-console.log(a); //[true, true, false, false]
-
-const b = numbers.filter(numbers =>number < 3);
-console.log(b); //[1, 2]
-
-console.log(numbers); //map, filter 모두 원본배열을 변화시키지 않는다.
+const a = fruits.find(fruit => {
+  return /^B/.test(fruit);
+});
+console.log(a);
