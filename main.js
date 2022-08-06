ffc0f876b4b91c72b2ad005fe93f8511e7c4e5e4
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:07:09 +0900

    map () return in different format

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ const a = fruits.forEach(function (fruit, index) {
 });
 console.log(a);
 
-const b = fruits.map(function (fruit, index) {
-  return `${fruit}-${index}`;
+const b = fruits.map(function (fruit, index) { //map 은 return값들을 토대로 "새로운 배열"을 만들어서 반환한다. 
+  return { //{} 리터럴이라는 새로운 배열을 만들어서 반환한다.
+    id: index,
+    name: fruit,
+  };
 });
 console.log(b);
