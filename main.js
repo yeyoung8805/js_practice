8110aba4045bf621077860339f3a5dc41e5537ea
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 12:40:52 +0900

    array length

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+//.length
+
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-console.log(numbers[1]);
-console.log(fruits[2]);
+console.log(numbers.length);
+console.log(fruits.length);
+console.log([1, 2].length);
