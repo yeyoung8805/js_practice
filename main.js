9fafe657ba8b202e1bff081571711f8419a2d113
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:43:12 +0900

    change the parameters in splice() method

--- main.js
+++ main.js
@@ -4,6 +4,6 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-numbers.splice(2, 1); //splice(index, count); = 인덱스번호 2번에서 1개를 지운다.
+numbers.splice(2, 2); //splice(index, count); = 인덱스번호 2번에서 2개를 지운다. 즉 3,4가 지워진다.
 
-console.log(numbers); // result : [1, 2, 4] 
+console.log(numbers); // result : [1, 2] 
