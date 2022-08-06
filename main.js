bc68e53a7da28c2d33852846490bbf88b9b0e68b
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 12:59:26 +0900

    index can be replaced to i

--- main.js
+++ main.js
@@ -3,6 +3,6 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-fruits.forEach(function (fruit, index) { //function ~부터는 콜백함수이다.
-	console.log(fruit, index); // 콘솔에 배열의 크기(length) 만큼 찍힌다.
+fruits.forEach(function (fruit, i) { //function ~부터는 콜백함수이다.
+	console.log(fruit, i); // 콘솔에 배열의 크기(length) 만큼 찍힌다.
 });
