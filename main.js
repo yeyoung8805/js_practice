582a733dcdb3b459e203f149abb116c81471e707
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:44:34 +0900

    delete 0 items in splice() method

--- main.js
+++ main.js
@@ -4,6 +4,6 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-numbers.splice(2, 2); //splice(index, count); = 인덱스번호 2번에서 2개를 지운다. 즉 3,4가 지워진다.
+numbers.splice(2, 0); //splice(index, count); = 인덱스번호 2번에서 0개를 지운다. 즉 지워지는 것이 없다,
 
-console.log(numbers); // result : [1, 2] 
+console.log(numbers); // result : [1, 2, 3, 4] 
