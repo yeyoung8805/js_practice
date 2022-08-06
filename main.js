a81ccf14acd427cdbe04b6132f481a454d91761b
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 12:58:01 +0900

    item can be changed to fruit

--- main.js
+++ main.js
@@ -3,6 +3,6 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-fruits.forEach(function (item, index, array) { //function ~부터는 콜백함수이다.
-	console.log(item, index, array); // 콘솔에 배열의 크기(length) 만큼 찍힌다.
+fruits.forEach(function (fruit, index, array) { //function ~부터는 콜백함수이다.
+	console.log(fruit, index, array); // 콘솔에 배열의 크기(length) 만큼 찍힌다.
 });
