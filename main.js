747739bc86517ac4078079a45d136b3cf7a49d1e
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:13:33 +0900

    array filter()

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-//.map()
+//.filter()
 
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-const a = fruits.forEach((fruit, index) => { 
-	console.log(`${fruit}-${index}`); 
+const a = numbers.map(numbers => { 
+	return number < 3;
 });
-console.log(a);
+console.log(a); //[true, true, false, false]
 
-const b = fruits.map((fruit, index) => ({ 
-  id: index,
-  name: fruit,
-}));
-console.log(b);
+const b = numbers.filter(numbers => {
+  return number < 3;
+});
+console.log(b); //[1, 2]
