dac256286b5428080b0c0f802f05ff23471bfe66
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:33:37 +0900

    array includes() method

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-//.find() .findIndex()
+//.includes()
 
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-const a = fruits.find(fruit => /^C/.test(fruit));
-console.log(a); //result : Cherry
+const a = numbers.includes(3);
+console.log(a); //result : true
 
-const b = fruits.findIndex(fruit => /^C/.test(fruit));
-console.log(b); //result : 2 (Cherry index is 2)
+const b = numbers.includes('Melon');
+console.log(b); //result : false
