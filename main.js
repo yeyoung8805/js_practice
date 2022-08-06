b9851b7e51cb3da681f5b514c11da73e5ee0d7a5
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:39:33 +0900

    array reverse() method changes backwards

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//.push() .unshift()
+//.reverse()
 //원본 배열이 수정되므로 주의!
 
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-numbers.push(5); //push() : 배열의 가장 뒤쪽에 데이터가 삽입된다.
-console.log(numbers);  //[1,2,3,4,5]
+numbers.reverse();
+fruits.reverse();
 
-numbers.unshift(0); //unshift() : 배열의 가장 앞쪽에 데이터가 삽입된다.
-console.log(numbers); //[0,1,2,3,4,5]
+console.log(numbers); // result:  [4,3,2,1]
+console.log(fruits); // result: ['Cherry', 'Banana', 'Apple']
