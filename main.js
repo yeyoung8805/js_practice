d79c625670c7b1b75773390ce3ea5bb916862d29
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:10:20 +0900

    changing normal function to arrow function form

--- main.js
+++ main.js
@@ -3,15 +3,13 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-const a = fruits.forEach(function (fruit, index) { //forEach() 는 따로 반환하는 값 return 이 없다.
+const a = fruits.forEach((fruit, index) => { //forEach() 는 따로 반환하는 값 return 이 없다.
 	console.log(`${fruit}-${index}`); 
 });
 console.log(a);
 
-const b = fruits.map(function (fruit, index) { //map 은 return값들을 토대로 "새로운 배열"을 만들어서 반환한다. 
-  return { //{} 리터럴이라는 새로운 배열을 만들어서 반환한다.
-    id: index,
-    name: fruit,
-  };
-});
+const b = fruits.map((fruit, index) => ({ //{} 리터럴이라는 새로운 배열을 만들어서 반환한다.
+  id: index,
+  name: fruit,
+}));
 console.log(b);
