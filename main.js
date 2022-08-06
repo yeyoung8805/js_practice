c4b7d73a95ba7ea429c490d76191e01e349fa3c3
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:29:52 +0900

    array findIndex() method

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ const a = fruits.find(fruit => {
   return /^C/.test(fruit); //정규표현식 : C로 시작(^)하는 문자데이터 첫번째배열 value
 });
 console.log(a); //result : Cherry
+
+const b = fruits.findIndex(fruit => {
+  return /^C/.test(fruit); 
+});
+console.log(b); //result : 2 (Cherry index is 2)
