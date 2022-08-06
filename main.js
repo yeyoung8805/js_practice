4cc1722bc578a21dbba285d3bd6870bffa2c1cd8
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:03:04 +0900

    array map() (compare with forEach() )

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-//.forEach()
+//.map()
 
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-fruits.forEach(function (fruit, i) { //function ~부터는 콜백함수이다.
-	console.log(fruit, i); // 콘솔에 배열의 크기(length) 만큼 찍힌다.
+const a = fruits.forEach(function (fruit, index) { 
+	console.log(`${fruit}-${index}`); 
 });
+console.log(a);
+
+const b = fruits.map(function (fruit, index) {
+  return `${fruit}-${index}`;
+});
+console.log(b);
