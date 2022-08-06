9a43ded03dcfbbc90abe908ca845b149c15568c8
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:42:08 +0900

    array splice() method

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-//.reverse()
+//.splice()
 //원본 배열이 수정되므로 주의!
 
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-numbers.reverse();
-fruits.reverse();
+numbers.splice(2, 1); //splice(index, count); = 인덱스번호 2번에서 1개를 지운다.
 
-console.log(numbers); // result:  [4,3,2,1]
-console.log(fruits); // result: ['Cherry', 'Banana', 'Apple']
+console.log(numbers); // result : [1, 2, 4] 
