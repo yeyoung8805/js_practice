47128c8b599f32896900fc0f2ffb9488687cf24e
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:31:24 +0900

    normal to arrow function

--- main.js
+++ main.js
@@ -3,12 +3,8 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-const a = fruits.find(fruit => {
-  return /^C/.test(fruit); //정규표현식 : C로 시작(^)하는 문자데이터 첫번째배열 value
-});
+const a = fruits.find(fruit => /^C/.test(fruit));
 console.log(a); //result : Cherry
 
-const b = fruits.findIndex(fruit => {
-  return /^C/.test(fruit); 
-});
+const b = fruits.findIndex(fruit => /^C/.test(fruit));
 console.log(b); //result : 2 (Cherry index is 2)
