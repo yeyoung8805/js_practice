4ff9c32d9df5a3686de56e9c036ba5bda8036f49
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:48:35 +0900

    add comment to using splice()

--- main.js
+++ main.js
@@ -4,6 +4,6 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-numbers.splice(2, 1, 999); //splice(index, count); = 인덱스번호 2번에서 1개를 지운다. 즉 3이 삭제된다. 그리고 999라는 숫자를 끼워넣는다.
+numbers.splice(2, 2, 999); //splice(index, count, addItem); = 인덱스번호 2번에서 2개를 지운다. 즉 3,4가 삭제된다. 그리고 그 자리에 999라는 숫자를 끼워넣는다.
 
-console.log(numbers); // result : [1, 2, 999, 4] 
+console.log(numbers); // result : [1, 2, 999] 
