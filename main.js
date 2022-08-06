4856e4e41c2a7cdb08139ff23e7ba0de29b760e5
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 12:44:06 +0900

    add comment to concat()

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //.concat()
+//두개의 배열을 병합한다. 단, 원본 배열에는 변화가 없다.
 
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-console.log(numbers.concat(fruits));
+console.log(numbers.concat(fruits)); // result : [1, 2, 3, 4, 'Apple', 'Banana', 'Cherry']
 console.log(numbers);
 console.log(fruits);
