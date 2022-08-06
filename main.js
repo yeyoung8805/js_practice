c13c4033f04ad4e0935fa740a2100c26bfeb5be7
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:18:42 +0900

    difference between map() and filter()

--- main.js
+++ main.js
@@ -3,12 +3,12 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-const a = numbers.map(numbers => { 
+const a = numbers.map(numbers => {  //배열의 개수만큼 boolean 값이 반환된다.
 	return number < 3;
 });
 console.log(a); //[true, true, false, false]
 
-const b = numbers.filter(numbers => {
+const b = numbers.filter(numbers => { //원본 배열에서 필터링해서 "새로운 배열"을 만들어 반환한다.
   return number < 3;
 });
 console.log(b); //[1, 2]
