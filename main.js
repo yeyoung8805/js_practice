09cc21974a772059aa8a0a4ee050c26c90ce2df9
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:47:00 +0900

    how to delete, and add item in splice()

--- main.js
+++ main.js
@@ -4,6 +4,6 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-numbers.splice(2, 0, 999); //splice(index, count); = 인덱스번호 2번에서 0개를 지운다. 그리고 999라는 숫자를 끼워넣는다.
+numbers.splice(2, 1, 999); //splice(index, count); = 인덱스번호 2번에서 1개를 지운다. 즉 3이 삭제된다. 그리고 999라는 숫자를 끼워넣는다.
 
-console.log(numbers); // result : [1, 2, 999, 3, 4] 
+console.log(numbers); // result : [1, 2, 999, 4] 
