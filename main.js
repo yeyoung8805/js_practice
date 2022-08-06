27aa28e67221656a6526dd0d69726426ae47a631
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:45:41 +0900

    how to add item in splice() method

--- main.js
+++ main.js
@@ -4,6 +4,6 @@
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-numbers.splice(2, 0); //splice(index, count); = 인덱스번호 2번에서 0개를 지운다. 즉 지워지는 것이 없다,
+numbers.splice(2, 0, 999); //splice(index, count); = 인덱스번호 2번에서 0개를 지운다. 그리고 999라는 숫자를 끼워넣는다.
 
-console.log(numbers); // result : [1, 2, 3, 4] 
+console.log(numbers); // result : [1, 2, 999, 3, 4] 
