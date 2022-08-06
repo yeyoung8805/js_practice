a274f33a8dae7b5ece4f897887b123660a0a31dc
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 13:37:08 +0900

    array push(), unshift() method

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-//.includes()
+//.push() .unshift()
+//원본 배열이 수정되므로 주의!
 
 const numbers = [1, 2, 3, 4];
 const fruits = ['Apple', 'Banana', 'Cherry'];
 
-const a = numbers.includes(3);
-console.log(a); //result : true
+numbers.push(5); //push() : 배열의 가장 뒤쪽에 데이터가 삽입된다.
+console.log(numbers);  //[1,2,3,4,5]
 
-const b = numbers.includes('Melon');
-console.log(b); //result : false
+numbers.unshift(0); //unshift() : 배열의 가장 앞쪽에 데이터가 삽입된다.
+console.log(numbers); //[0,1,2,3,4,5]
