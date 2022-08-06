85bdc30981ed93d84525c19b9fa53ae1f4b23acd
“yeyoung8805” <yyk8805@naver.com>
2022-08-06 12:33:40 +0900

    array (number, String)

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-console.log('abs: ', Math.abs(-12)); //abs : 절대값
+const numbers = [1, 2, 3, 4];
+const fruits = ['Apple', 'Banana', 'Cherry'];
 
-console.log('min: ', Math.min(2, 8));
-
-console.log('max: ', Math.max(2, 8));
-
-console.log('ceil: ', Math.ceil(3.14)); //ceil : 소수점 첫째자리에서 정수자리로 올림처리
-
-console.log('floor: ', Math.floor(3.14)); //floor :소수점 첫째자리에서 내림처리하여 정수만 남김
-
-console.log('round: ', Math.round(3.14)); //round : 정수를 기준으로 반올림. 예를 들어 3.66 -> 4가 된다.
-
-console.log('random: ', Math.random()); //random : 난수를 반환
+console.log(numbers[1]);
+console.log(fruits[2]);
