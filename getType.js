02d18d1fb8f0ce2bcde49da2630c0c564931b396
yeyoung8805 <yyk8805@naver.com>
2022-08-08 14:52:15 +0900

    wrong sentence

--- getType.js
+++ getType.js
@@ -1,3 +1,4 @@
 export default function getType(data) { //내보내지는(export) 가장 기본적인(default) 함수라고 선언
   return Object.prototype.toString.call(data).slice(8, -1);
 }
+// export default 123; //wrong : only one export allowed per module
