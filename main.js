c8a122b66ebcf6a0627bf83588caa018adf4feaf
yeyoung8805 <yyk8805@naver.com>
2022-08-08 10:26:54 +0900

    Destructuring assignment in array data

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ console.log(`${yeyoung}의 나이는 ${age}세입니다.`);
 console.log(`${yeyoung}의 이메일 주소는 ${user.email} 입니다.`); //구조분해할당 되지 않았을때 email 키의 밸류값을 도출하기 위한 방법이다.
 console.log(address); //address default값 Korea가 할당돼있지만, const address에서 Singapore 가 할당돼있으므로 Singapore가 출력된다.
 
+// 객체데이터 뿐만아니라 '배열 데이터'도 구조분해할당 가능하다!
 const fruits = ['Apple', 'Banana', 'Cherry'];
-const [a,b,c,d] = fruits;
+const [a,b,c,d] = fruits; //배열의 이름으로 추출하는게 아니라 a,b,c,d 라는 순서대로 추출한다.
 console.log(a,b,c,d);
