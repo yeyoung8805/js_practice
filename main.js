f540027a7e94bd1195d36e77df04aee511253daf
yeyoung8805 <yyk8805@naver.com>
2022-08-08 14:54:46 +0900

    import using alias  "as" keyword

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import _ from 'lodash'; //from `node_modules`!
 import getType from './getType'; //getType.js //default 키워드가 붙은 파일은 하나의 파일에 하나의 export만 내보낼 수 있다!
-import {random, user} from './getRandom'; //getRandom.js //default 키워드가 빠진 파일은 여러개의 export 작성 가능!
+import {random, user as yeyoung} from './getRandom'; //getRandom.js //default 키워드가 빠진 파일은 여러개의 export 작성 가능!
 
 console.log(_.camelCase('the hello world')); //theHelloWorld
 console.log(getType([1,2,3])); //Array
 console.log(random(), random()); //randomly changed two numbers
-console.log(user);
+console.log(yeyoung);
