a755fab2d2822bcdcdab22d7f39b0c496495e04c
yeyoung8805 <yyk8805@naver.com>
2022-08-08 09:50:24 +0900

    usage of map() in keys array data

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const user = {
   email: 'yyk8805@naver.com'
 };
 
-const keys = Object.keys(user);
+const keys = Object.keys(user); //key(property)만 추출해내어 배열을 만듦
 console.log(keys);
 // ['name', 'age', 'email']
 
-console.log(user['email']);
+console.log(user['email']); //email 이라는 key의 value를 받아오고자 할때
 
-const values = keys.map(key => user[key]);
+const values = keys.map(key => user[key]); //keys 라는 배열 데이터에는 map()을 사용할 수 있다.
+                                           //map(콜백함수) : 3번 실행되는, 콜백함수 결과가 map이라는 매개변수에 들어간다.
 console.log(values);
