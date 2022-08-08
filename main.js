a06e0bb931fd748b2202267fe2e4a22375fe08d1
yeyoung8805 <yyk8805@naver.com>
2022-08-08 10:17:09 +0900

    example of user.email

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ const user = {
   email: 'yyk8805@naver.com'
 };
 
-const {name, age, email, address} = user;
+const {name, age} = user; //구조분해할당 (=비구조화 할당)의 예
 //E.g, user.address
 
 console.log(`사용자의 이름은 ${name} 입니다.`);
 console.log(`${name}의 나이는 ${age}세입니다.`);
-console.log(`${name}의 이메일 주소는 ${email} 입니다.`);
-console.log(address);
+console.log(`${name}의 이메일 주소는 ${user.email} 입니다.`); //구조분해할당 되지 않았을때 email 키의 밸류값을 도출하기 위한 방법이다.
+console.log(user.address);
 
 const fruits = ['Apple', 'Banana', 'Cherry'];
 const [a,b,c,d] = fruits;
