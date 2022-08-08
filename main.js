58472d225a823003370260212c57b0d1491bc956
yeyoung8805 <yyk8805@naver.com>
2022-08-08 10:36:34 +0900

    Spread operator

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
-//구조 분해 할당 (Destructuring assignment)
-// = 비구조화 할당
+// 전개연산자 (Spread)
 
-const user = {
-  name: 'Yeyoung',
-  age: 88,
-  email: 'yyk8805@naver.com',
-  address: 'Singapore'
-};
-
-const {name: yeyoung, age, address = 'Korea'} = user; //구조분해할당 (=비구조화 할당)의 예
-//E.g, user.address
-
-console.log(`사용자의 이름은 ${yeyoung} 입니다.`);
-console.log(`${yeyoung}의 나이는 ${age}세입니다.`);
-console.log(`${yeyoung}의 이메일 주소는 ${user.email} 입니다.`); //구조분해할당 되지 않았을때 email 키의 밸류값을 도출하기 위한 방법이다.
-console.log(address); //address default값 Korea가 할당돼있지만, const address에서 Singapore 가 할당돼있으므로 Singapore가 출력된다.
-
-// 객체데이터 뿐만아니라 '배열 데이터'도 구조분해할당 가능하다!
 const fruits = ['Apple', 'Banana', 'Cherry'];
-const [, ,b] = fruits; //a는 필요없지만 b가 두번째 순서이기 때문에 앞에 쉼표(,)는 남겨둬야 한다!!
-console.log(b);
+console.log(fruits);
+console.log(...fruits); //전개연산자 사용
+// console.log('Apple', 'Banana', 'Cherry');
+
+function toObject(a, b, c) {
+  return {
+    a: a,
+    b: b,
+    c: c
+  }
+}
+console.log(toObject(...fruits));
