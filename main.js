b9ba8ed85fe085b443b9aef8e2e6e13da574498e
yeyoung8805 <yyk8805@naver.com>
2022-08-08 10:20:55 +0900

    adding const user

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 const user = {
   name: 'Yeyoung',
   age: 88,
-  email: 'yyk8805@naver.com'
+  email: 'yyk8805@naver.com',
+  address: 'Singapore'
 };
 
 const {name, age, address = 'Korea'} = user; //구조분해할당 (=비구조화 할당)의 예
@@ -13,7 +14,7 @@ const {name, age, address = 'Korea'} = user; //구조분해할당 (=비구조화
 console.log(`사용자의 이름은 ${name} 입니다.`);
 console.log(`${name}의 나이는 ${age}세입니다.`);
 console.log(`${name}의 이메일 주소는 ${user.email} 입니다.`); //구조분해할당 되지 않았을때 email 키의 밸류값을 도출하기 위한 방법이다.
-console.log(address); //const user에는 address 값이 할당되지 않았지만, 디폴트 값 'Korea' 가 할당된 상태이므로 Korea가 출력된다.
+console.log(address); //address default값 Korea가 할당돼있지만, const address에서 Singapore 가 할당돼있으므로 Singapore가 출력된다.
 
 const fruits = ['Apple', 'Banana', 'Cherry'];
 const [a,b,c,d] = fruits;
