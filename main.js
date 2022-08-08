facd1b284c3c334a7b95a2c51cff66f489d89c3b
yeyoung8805 <yyk8805@naver.com>
2022-08-08 15:58:06 +0900

    properties with single quotation

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //JSON (JavaScript Object Notation)
 //자바스크립트의 객체 표기법
 const user = {
-  name: "HEROPY",
-  age: 85,
-  emails: [
+  'name': "HEROPY",
+  'age': 85,
+  'emails': [
     'thesecon@gmailcom',
     'neo@zillinks.com'
   ],
