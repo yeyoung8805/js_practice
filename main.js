64b3d5f56441ff41cecdbafa2828f5af06657ee5
yeyoung8805 <yyk8805@naver.com>
2022-08-08 14:14:42 +0900

    applying deep copy by lodash

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 //데이터 불변성 (Immutability)
 //원시데이터 : String, Number, Boolean, undefined, null
 //참조형 데이터 : Object, Array, Function
@@ -11,7 +13,7 @@ const user = {
   emails: ['yyk8805@naver.com']
 }
 
-const copyUser = {...user}; //전개(Spread)연산자 : 빈 {}객체에 user객체의 속성, 값들을 전개해서 넣는다.
+const copyUser = _.cloneDeep(user); //깊은복사(deep copy) 하는 방법이다!!
 console.log(copyUser === user); //false
 
 user.age = 22;
@@ -21,7 +23,7 @@ console.log('copyUser', copyUser);
 console.log('------')
 console.log('------')
 
-user.emails.push('aaa@naver.com'); //user라는 객체의 emails라는 배열 데이터도 참조형이다. 따라서 emails라는 배열은 기존과 같은 메모리 주소를 참조하고 있다.
-console.log(user.emails === copyUser.emails); //true
+user.emails.push('aaa@naver.com');
+console.log(user.emails === copyUser.emails); //false
 console.log('user', user);
 console.log('copyUser', copyUser);
