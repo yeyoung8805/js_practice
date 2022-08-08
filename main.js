dece078bea528d88c27feadb262afbdd7924a40b
yeyoung8805 <yyk8805@naver.com>
2022-08-08 10:55:05 +0900

    data Immutability

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// 전개연산자 (Spread)
+//데이터 불변성 (Immutability)
+//원시데이터 : String, Number, Boolean, undefined, null
+//참조형 데이터 : Object, Array, Function
 
-const fruits = ['Apple', 'Banana', 'Cherry', 'Orange']; //Orange 항목을 추가했다.
-console.log(fruits);
-console.log(...fruits); //전개연산자 사용
-
-const toObject = (a, b, ...c) => ({a, b, c}); //화살표 함수 내에서 배열데이터를 반환하려면 ()로 한번 감싸야 한다!!
-console.log(toObject(...fruits)); //c변수엔 배열의 형태로 밸류값들이 들어간다.
+let a = 1;
+let b = 4;
+console.log(a, b, a === b); // 1 4 false
+b = a;
+console.log(a, b, a === b); // 1 1 true
+a = 7;
+console.log(a, b, a === b); // 7 1 false
+let c = 1;
+console.log(b, c, b === c); // 1 1 true
