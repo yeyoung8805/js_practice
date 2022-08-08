142633baff4b952a1ba6f5f22f639e0a1ffe7123
yeyoung8805 <yyk8805@naver.com>
2022-08-08 14:50:09 +0900

    rm default can make the file export more modules

--- getRandom.js
+++ getRandom.js
@@ -1,3 +1,8 @@
 export function random() { //default 키워드를 뺐으므로, import 해올때 {함수명}으로 가져와야 한다.
   return Math.floor(Math.random() * 10); // Math.random() * 10 (정수 1 자리수를 받아와서) -> floor () (내림을 하는것(소수점은 버리는것))
 }
+
+export const user = {
+  name: 'Yeyoung',
+  age: 88
+};
