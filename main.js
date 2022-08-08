87e77f19a1ff0a1593d0904f2b59df88e6ce9f18
yeyoung8805 <yyk8805@naver.com>
2022-08-08 10:47:27 +0900

    abbreviate form of property and variable in return

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ console.log(fruits);
 console.log(...fruits); //전개연산자 사용
 
 function toObject(a, b, ...c) { //...c는 "Rest parameter"로서 나머지 변수들을 모두 포함한다.
-  return {
-    a: a,
-    b: b,
-    c: c
+  return { //속성의 이름과 변수의 이름이 같을 때 a:a를 a로 단순하게 나타낼 수 있다.
+    a,
+    b,
+    c
   }
 }
 console.log(toObject(...fruits)); //c변수엔 배열의 형태로 밸류값들이 들어간다.
