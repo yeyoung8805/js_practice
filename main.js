cf3f630fc1124fa59f86d9d81ca43b80d8960b99
yeyoung8805 <yyk8805@naver.com>
2022-08-08 16:09:11 +0900

    stringify() and parse() of JSON

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ const user = {
 
 console.log('user', user);
 
-const str = JSON.stringify(user);
+const str = JSON.stringify(user); //stringify() : JSON 형태로 문자화 시킴
 console.log('str', str); // {"name":"HEROPY","age":85,"emails":["thesecon@gmailcom","neo@zillinks.com"]}
 console.log(typeof str); // string
 
-const obj = JSON.parse(str);
+const obj = JSON.parse(str); //parse() : 문자데이터를 자바스크립트에서 활용할 수 있는 실제 데이터로 변경시켜준다.
 console.log('obj', obj);
