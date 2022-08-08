f23ec15da0c87847dd6737ab3b5dab274bfe622e
yeyoung8805 <yyk8805@naver.com>
2022-08-08 13:39:35 +0900

    shallow copy, deep copy

--- main.js
+++ main.js
@@ -4,15 +4,19 @@
 
 //참조형 데이터
 //얕은 복사와 깊은 복사 개념 이해 필요
-let a = {k: 1};
-let b = {k: 1};
-console.log(a, b, a === b); //false : 왜냐하면 서로 다른 메모리 주소를 바라보고 있다.
-a.k = 7;
-b = a;
-console.log(a, b, a === b); //true : a의 메모리 주소를 b가 할당받기 때문에 똑같다.
-a.k = 2;
-console.log(a, b, a === b); //true : a라는 변수의 k속성을 바꾼거지만, 이미 a와 b는 같은 메모리 주소를 바라보고 있기 때문에 똑같다.
-let c = b;
-console.log(a, b, c, a === c); //true : a,b,c 모두 같은 메모리 주소를 바라보고 있기 때문에 똑같다.
-a.k = 9;
-console.log(a, b, c, a === c); //true : a라는 변수의 k속성은 바꾸었고, 이미 a,b,c는 같은 메모리 주소를 바라보고 있기 때문에 똑같다.
+//얕은 복사(shallow copy), 깊은 복사(deep copy)
+const user = {
+  name: 'yeyoung',
+  age: 88,
+  emails: ['yyk8805@naver.com']
+}
+
+const copyUser = user;
+console.log(copyUser === user); //true
+
+user.age = 22;
+console.log('user', user);
+console.log('copyUser', copyUser);
+
+console.log('------')
+console.log('------')
